b4efde9f2ff42029a063abe47215872c2169550a
Sergio Joya <sejoya104@gmail.com>
2024-06-29 20:25:55 -0400

    Add logic to get users choice

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Create a function that generates a number from 0-2
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -10,3 +9,25 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+function getHumanChoice() {
+    const userChoice = prompt("Rock, paper, scissors...");
+    if (userChoice || userChoice === "") {
+        switch (userChoice.toLowerCase()) {
+            case "rock":
+                return "rock";
+            case "paper":
+                return "paper";
+            case "scissors":
+                return "scissors";
+            default:
+                window.alert("Invalid input. Please try again");
+                return getHumanChoice();
+        }
+    } else {
+        return;
+    }
+}
+
+console.log(getHumanChoice());
+// console.log(getComputerChoice());
